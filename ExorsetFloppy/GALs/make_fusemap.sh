22b03882be8ec4c525c0e0f10eba57362785ed2f
roberto314 <100proentoffner@gmail.com>
2025-12-30 12:53:52 +0100

    first test with exordisk board, GALs new

--- ExorsetFloppy/GALs/make_fusemap.sh
+++ ExorsetFloppy/GALs/make_fusemap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 galasm="./galasm"
-
+echo "--------------------------------"
 $galasm -c G1.eqn
+echo "--------------------------------"
 $galasm -c G2.eqn
---
+++ ExorsetFloppy/GALs/prog_16v8.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+minipro="/usr/local/bin/minipro"
+gal="GAL16V8D"
+#gal="GAL22V10D"
+
+${minipro}  --device ${gal} -P --write G2.jed
